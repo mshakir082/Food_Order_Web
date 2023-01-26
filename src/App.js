a1a01143fec8d4ee84d109a803e5264d84dac9a2
YCTechDev <[yctech@yellowchalk.com]>
2023-01-26 15:38:32 +0530

    meals Lists

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react";
 import Header from "./Components/Layout/Header";
 import MealsSummary from "./Components/Meals/MealsSummary";
-
+import Meals from "./Components/Meals/Meals";
 function App() {
   return (
- <Fragment>
-  <Header />
-  <MealsSummary />
-</Fragment>
+    <Fragment>
+    <Header />
+    <main>
+      <Meals />
+    </main>
+  </Fragment>
   );
 }
 
--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,8 +1,45 @@
-import React from "react";
-const AvailableMeals = () => {
+import classes from "./AvailableMeals.module.css";
+ 
+const DUMMY_MEALS = [
+    {
+      id: 'm1',
+      name: 'Butter Naan ',
+      description: ' maida naan dipped in butter     ',
+      price: 22.99,
+    },
+    {
+      id: 'm2',
+      name: 'Daal Makhni ',
+      description: ' Daal with loded butter and tadka   ',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      name: 'Kadhai paneer ',
+      description: ' Paneer and various veggies     ',
+      price: 12.99,
+    },
+    {
+      id: 'm4',
+      name: 'Veg Kofta ',
+      description: ' made from bottlegaurd     ',
+      price: 18.99,
+    },
+  ];
+  const AvailableMeals = () => {
 
-    <>
-    <div></div>
-    </>
+  const mealsList = DUMMY_MEALS.map(meal => 
+
+         <li>
+         {meal.name} 
+         {meal.description}
+         {meal.price}
+         </li> 
+   );
+return <section className={classes.meals}>
+    <ul className={classes.mealsList}>
+        {mealsList}
+    </ul>
+</section>
 }
 export default AvailableMeals;
