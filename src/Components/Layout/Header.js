99f80f7d3988e9bc45c89476a24980194036ba9a
YCTechDev <[yctech@yellowchalk.com]>
2023-01-26 13:12:08 +0530

    Header Part Create

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,23 @@
+import React, {Fragment} from "react";
+import mealsImage from '../../Assets/restaurant-2.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+import food_logo  from '../../Hussain_Food_Logo.png'
+const Header = props => {
+    return(
+     <Fragment>
+        <header className={classes.header} >
+           <div style={{display:'flex',justifyContent:'center',width:'15%'}}> 
+            <img src={food_logo} alt='logo' style={{width:'30%',height:'15%',margin:'auto'}}/>
+            <h3 className={classes.header_logo_title}>Hussain Food</h3>
+           </div>
+            <HeaderCartButton />
+        </header>
+        <div className={classes['main-image']}>
+            <img src = {'https://b.zmtcdn.com/mx-onboarding-hero87f77501659a5656cad54d98e72bf0d81627911821.webp'} alt= "restaurant_image "></img>
+        </div>
+    </Fragment>
+    )
+}
+
+export default Header;
---
+++ src/Components/Layout/HeaderCartButton.js
@@ -0,0 +1,15 @@
+import CartIcon from "../Cart/CartIcon";
+import classes from './HeaderCartbutton.module.css'
+
+
+const HeaderCartButton = props => {
+    return <button className={classes.button}>
+        <span className={classes.icon}>
+            <CartIcon />
+        </span>
+        <span> Cart</span>
+        <span className={classes.badge}> 7</span>
+    </button>
+
+}
+export default HeaderCartButton;
